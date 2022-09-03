1aa51d1fc569f15edd3909991db08017a63864b4
amirhoseinsoleimannejad <amhsoleimannejad@gmail.com>
2022-09-03 16:45:45 +0430

    article crud

--- database/migrations/2022_09_03_072609_create_articles_table.php
+++ database/migrations/2022_09_03_072609_create_articles_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title',128);
+            $table->string('writer_name',128)->nullable();
+            $table->string('body')->nullable();
+            $table->string('conclusion')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use \App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,65 +21,12 @@ Route::get('/', function () {
 
 
 
+Route::resource('articles', ArticleController::class);
 
 
 
+//Route::get('articles', [ArticleController::class, 'index'])->name('articles');
+//Route::post('store-form', [ArticleController::class, 'store']);
 
-//Route::redirect('/test/', '/test2/');
-
-
-Route::view('/welcome', 'welcome');
-
-
-
-Route::get('/rrr', function (Request $request) {
-
-
-});
-
-
-
-Route::get('/search/{search}', function ($search) {
-    return $search;
-})->where('search', 'salam');
-
-
-
-
-
-
-
-Route::prefix('admin')->group(function () {
-
-
-    Route::get('/user/profile', function () {
-        return "kiiir";
-    })->name('profile');
-
-
-
-    Route::match(['get', 'post'], '/test2/', function () {
-        $url = route('profile');
-        return $url;
-    });
-
-
-
-    Route::any('/test/', function () {
-        return 'salam laravel';
-    });
-
-});
-
-
-
-
-
-
-Route::name('admin.')->group(function () {
-    Route::get('/users', function () {
-        return 'gggg';
-    })->name('users');
-});
 
 
